5fe8b4fe6f6b98671e702ba39b2adf6947586c6a
Paixram <lucho1996g@gmail.com>
2024-02-01 04:08:01 -0500

    Update funcs

--- src/main/java/com/pooespol/p_poo/ReservaVuelo5Controller.java
+++ src/main/java/com/pooespol/p_poo/ReservaVuelo5Controller.java
@@ -6,7 +6,10 @@ package com.pooespol.p_poo;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
 /**
  * FXML Controller class
@@ -14,13 +17,32 @@ import javafx.fxml.Initializable;
  * @author Luizzz
  */
 public class ReservaVuelo5Controller implements Initializable {
-
+    
+    @FXML
+    VBox biboz;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        cargarVentana();
     }    
+    
+    public void cargarVentana() {
+        Label la = new Label("Vuelho de yda: " + VentanaReservaController.salida + ":");
+        Label lo = new Label(VentanaReservaController.origen + " a " + VentanaReservaController.destino);
+        
+        /// BOX LEIBOL
+        VBox bibox_deida = new VBox();
+        //Label hora = new Label("Duracion: " + VentanaReservaController.salida.toGMTString());
+        
+        
+        Label la2 = new Label("Vuelho de regrezho: " + VentanaReservaController.regreso + ":");
+        Label lo2 = new Label(VentanaReservaController.destino + " a " + VentanaReservaController.origen);
+        
+        biboz.getChildren().addAll(la, lo, la2, lo2);
+    }
     
 }
